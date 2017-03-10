5572c330a6f616f94ede2f4e77de80a766534b3b
Saulo <saulocn@gmail.com>
2017-03-09 23:40:04 -0300

    - Início de projeto com Vue JS e back-end em NodeJS;

---
+++ alurapic/src/App.vue
@@ -0,0 +1,91 @@
+
+<template>
+    <div class="corpo">
+        <h1 class="centralizado">{{titulo}}</h1>
+        <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="Filtre por parte do título" />
+        {{filtro}}
+        <!--img v-bind:src="foto.url" v-bind:alt="foto.titulo" /-->
+        <ul class="lista-fotos">
+            <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
+                <meu-painel :titulo="foto.titulo">
+                    <!--img class="imagem-rsponsiva" :src="foto.url" :alt="foto.titulo" /-->
+                    <imagem-responsiva :url="foto.url" :titulo="foto.titulo"></imagem-responsiva>
+                </meu-painel>
+            </li>
+        </ul>
+        
+        
+    </div>
+</template>
+
+
+<script>
+import Painel from './components/shared/painel/Painel.vue';
+import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue';
+export default {
+    components : {
+        'meu-painel' : Painel,
+        'imagem-responsiva' : ImagemResponsiva
+    },
+    data(){
+        return {
+            titulo : "Alurapic",
+            fotos:[],
+            filtro: '',
+        }
+    },
+
+    computed : {
+        fotosComFiltro(){
+            if(this.filtro){
+                /* filtrar */
+                let exp = new RegExp(this.filtro.trim(), 'i');
+                return this.fotos.filter(foto => exp.test(foto.titulo));
+            } else {
+                return this.fotos;
+            }
+        }
+    },
+
+    created(){
+        //alert("Componente criado!");
+        //let promise = this.$http.get("http://localhost:3000/v1/fotos/");
+        //promise.then(res => console.log(res));
+        /*promise.then(res =>  {
+            res.json().then(fotos=> this.fotos = fotos);
+        });*/
+        this.$http.get("http://localhost:3000/v1/fotos/")
+            .then(res => res.json())
+            .then(fotos => this.fotos = fotos, err => console.log(err));
+
+    }
+
+}
+
+</script>
+
+
+<style>
+    .corpo {
+        font-family: Helvetica, sans-serif;
+        width: 96%;
+        margin: 0 auto;
+    }
+
+    .centralizado {
+        text-align: center;
+    }
+
+    .lista-fotos {
+        list-style: none;
+    }
+
+    .lista-fotos .lista-fotos-item {
+        display: inline-block;
+    }
+
+    .filtro {
+        display: block;
+        width: 100%;
+    }
+</style>
